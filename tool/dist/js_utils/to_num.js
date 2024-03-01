71932fa16b2a93ae58bbf3a476a343a30bbc0e0c
gim_z66 <gimholg1992@foxmail.com>
2024-03-01 09:41:55 +0800

    .

--- tool/dist/js_utils/to_num.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.to_num = void 0;
-function to_num(v, or) {
-    var t = Number(v);
-    return Number.isNaN(t) ? v : (or === void 0) ? t : or;
-}
-exports.to_num = to_num;
