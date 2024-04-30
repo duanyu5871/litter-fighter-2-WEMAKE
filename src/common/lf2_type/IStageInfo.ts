d00543cc36d48441e514beac1a99520ccd27ceec
gim_z66 <gimholg1992@foxmail.com>
2024-04-30 19:17:08 +0800

    。

--- src/common/lf2_type/IStageInfo.ts
+++ src/common/lf2_type/IStageInfo.ts
@@ -1,9 +1,29 @@
 import { IStagePhaseInfo } from "./IStagePhaseInfo";
-
+/**
+ * 关卡信息
+ */
 export interface IStageInfo {
   bg: string;
   id: string;
   name: string;
   phases: IStagePhaseInfo[];
   next?: string;
+
+  /**
+   * 关卡结束判定
+   * 
+   * 默认是全部阶段已结束
+   *
+   * @type {?string} 
+   */
+  cond_end?: string;
+
+  /**
+   * 关卡结束后，如何才进入下一关卡
+   * 
+   * 默认是玩家跑到场景的最右边
+   *
+   * @type {?string}
+   */
+  act_of_goto_next?: string;
 }
--- src/common/lf2_type/IStagePhaseInfo.ts
+++ src/common/lf2_type/IStagePhaseInfo.ts
@@ -1,5 +1,11 @@
 import { IStageObjectInfo } from "./IStageObjectInfo";
 
+/**
+ * 关卡阶段信息
+ *
+ * @export
+ * @interface IStagePhaseInfo
+ */
 export interface IStagePhaseInfo {
   bound: number;
   desc: string;
@@ -13,11 +19,31 @@ export interface IStagePhaseInfo {
    */
   cam_jump_to_x?: number;
 
-  
+
   /**
    * 玩家跳至位置
    *
    * @type {?number}
    */
   player_jump_to_x?: number;
+
+
+  /**
+   * 阶段结束判定
+   * 
+   * 默认是场上无敌人
+   *
+   * @type {?string} 
+   */
+  end_condition?: string;
+
+
+  /**
+   * 阶段结束动作
+   * 
+   * 默认是进入下一阶段
+   *
+   * @type {?string}
+   */
+  end_action?: string;
 }
