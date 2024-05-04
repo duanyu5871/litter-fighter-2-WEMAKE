e27caf34c18d27718bb6ed0b154ec6c1638580b2
gim_mipro <gimholg1992@foxmail.com>
2024-05-04 09:56:02 +0800

    .

--- src/common/arithmetic_progression.ts
+++ src/common/arithmetic_progression.ts
@@ -1,7 +1,21 @@
+/**
+ * 获得一个等差数列
+ *
+ * @param {number} from 起点值，数列起点
+ * @param {number} to 终点值（不一定包含在数列中）
+ * @param {number} [gap=1] 差值
+ * @throws 当无法到达终点值，将抛出Error:[arithmetic_progression] dead loop!
+ * @returns {number[]} 等差数列
+ */
 export const arithmetic_progression = (from: number, to: number, gap: number = 1): number[] => {
+  if (gap === 0 || (to - from) / gap < 0)
+    throw new Error('[arithmetic_progression] dead loop!')
+
   const ret = [];
-  for (let i = from; i <= to; i += gap) {
-    ret.push(i);
+  for (let i = 0; true; ++i) {
+    const v = from + i * gap;
+    if ((gap > 0) ? (v >= to) : (v <= to)) break;
+    ret.push(v);
   }
   return ret;
 };
