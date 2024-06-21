a0df308750ffa0857933cf63816719ee0ecac7da
gim <gimholg1992@foxmail.com>
2024-06-22 02:08:18 +0800

    。

--- deployer/src/utils/progress_log.ts
+++ deployer/src/utils/progress_log.ts
@@ -15,8 +15,8 @@ export const progress_log = (verbose: any, ...args: any[]) => {
 }
 export const progress_log_end = (verbose: any, ...args: any[]) => {
   if (verbose) return console.log(...args);
-  
-  let line = '';
+
+  let line = args.join(' ');
   let pre_line_len = last_line_len;
   while (pre_line_len > line.length)
     line += ' '
