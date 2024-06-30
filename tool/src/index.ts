fa49c91ad5b965c74dafb215ae3048f664e7e327
gim_z66 <gimholg1992@foxmail.com>
2024-06-30 17:35:07 +0800

    .

--- tool/src/index.ts
+++ tool/src/index.ts
@@ -1,16 +1,12 @@
-import { spawn } from 'child_process';
 import command_exists from 'command-exists';
 import fs, { readFile } from 'fs/promises';
-import path from 'path';
-import dat_to_json from '../../src/LF2/dat_translator/dat_2_json';
-import { read_indexes } from '../../src/LF2/dat_translator/read_indexes';
-import { ICharacterData, IDataLists, IEntityPictureInfo } from '../../src/LF2/defines';
+import { IEntityPictureInfo } from '../../src/LF2/defines';
 import { exec_cmd } from './exec_cmd';
 import { check_is_str_ok } from './private/check_is_str_ok';
-import { read_lf2_dat_file } from './read_old_lf2_dat_file';
 import { classify } from './utils/classify';
 import { convert_dat_file } from './utils/convert_dat_file';
 import { convert_data_txt } from './utils/convert_data_txt';
+import { convert_sound } from './utils/convert_sound';
 import { make_zip_and_json } from './utils/make_zip_and_json';
 import { write_file } from './utils/write_file';
 
@@ -48,135 +44,22 @@ for (let i = 2; i < process.argv.length; ++i) {
       break;
   }
 }
-
-async function parse_under_dir(src_dir_path: string, dst_dir_path: string, indexes: IDataLists | undefined) {
-  if (steps.del_old) await fs.rmdir(dst_dir_path, { recursive: true }).catch(_ => void 0)
-  await fs.mkdir(dst_dir_path, { recursive: true }).catch(_ => void 0)
-
-  for (const filename of await fs.readdir(src_dir_path)) {
-    const src_path = path.join(src_dir_path, filename).replace(/\\/g, '/')
-    const stat = await fs.stat(src_path);
-    const dst_path = path.join(dst_dir_path, filename).replace(/\\/g, '/')
-    if (stat.isDirectory()) {
-      await parse_under_dir(src_path, dst_path, indexes)
-      continue;
-    }
-    if (!stat.isFile()) continue;
-
-    if (filename.endsWith('.exe')) {
-      console.log('ignored:', filename)
-      continue;
-    }
-    if (filename.endsWith('.dat')) {
-      if (!steps.dat) continue;
-      const text = await read_lf2_dat_file(src_path);
-      const index =
-        indexes?.objects.find(v => src_path.endsWith(v.file)) ||
-        indexes?.backgrounds.find(v => src_path.endsWith(v.file))
-      const json = dat_to_json(text, index);
-      {
-        let dirty = json as Partial<ICharacterData>
-        if (dirty?.frames?.[3]?.opoint) delete dirty.frames[3].opoint
-      }
-      if (!json) {
-        console.log('copy', src_path, '=>', dst_path)
-        await fs.copyFile(src_path, dst_path);
-        continue;
-      }
-
-      console.log('convert', src_path, '=>', dst_path)
-      await fs.writeFile(
-        dst_path.replace(/\.dat$/, '.json'),
-        JSON.stringify(json, null, 2)
-      )
-
-    } else if (filename.endsWith('.wma') || filename.endsWith('.wav')) {
-      if (!steps.sound) continue;
-      const _dst_path = dst_path + '.ogg'
-      const dst_stat = await fs.stat(_dst_path).catch(e => void 0);
-      if (dst_stat?.isFile() || dst_stat?.isDirectory())
-        await fs.rm(_dst_path, { recursive: true, force: true })
-      const args = ['-i', src_path, '-codec:a', 'libvorbis', '-b:a', '64k', '-ar', '44100', _dst_path];
-
-      if (!command_exists.sync('ffmpeg'))
-        throw new Error("ffmpeg not found, download it from: https://ffmpeg.org/download.html")
-
-      await new Promise((resolve, reject) => {
-        console.log('convert', src_path, '=>', _dst_path)
-        const temp = spawn('ffmpeg', args).on('exit', resolve).on('error', reject)
-        temp.stderr.on('data', d => console.error(filename, 'stderr: ' + d));
-      })
-    } else if (filename.endsWith('.bmp')) {
-      if (!steps.bmp) continue;
-      const _dst_path = dst_path.replace(/.bmp$/, '.png');
-      const dst_stat = await fs.stat(_dst_path).catch(e => void 0);
-      if (dst_stat?.isFile() || dst_stat?.isDirectory())
-        await fs.rm(_dst_path, { recursive: true, force: true })
-      const args = [src_path,
-        "-alpha",
-        "set",
-        "-fuzz", "0%",
-        "-fill", "rgba(0,0,0,0)",
-        "-opaque", "rgb(0,0,0)",
-        _dst_path
-      ];
-
-      if (!command_exists.sync('magick'))
-        throw new Error("magick not found, download it from: https://imagemagick.org/script/download.php")
-
-      await new Promise((resolve, reject) => {
-        console.log('convert', src_path, '=>', _dst_path)
-        const temp = spawn('magick', args).on('exit', resolve).on('error', reject)
-        temp.stderr.on('data', d => console.error(filename, 'stderr: ' + d));
-      })
-    } else if (steps.others) {
-      console.log('copy', src_path, '=>', dst_path)
-      await fs.copyFile(src_path, dst_path);
-    }
-  }
-}
 async function main() {
   const {
-    RAW_LF2_PATH, OUT_DIR,
-    DATA_DIR_PATH, DATA_ZIP_NAME,
-    PREL_DIR_PATH, PREL_ZIP_NAME,
+    RAW_LF2_PATH, DATA_DIR_PATH, OUT_DIR, DATA_ZIP_NAME, PREL_DIR_PATH, PREL_ZIP_NAME
   } = await readFile('./converter.config.json').then(buf => JSON.parse(buf.toString()))
-
   check_is_str_ok(
     RAW_LF2_PATH, OUT_DIR,
     DATA_DIR_PATH, DATA_ZIP_NAME,
     PREL_DIR_PATH, PREL_ZIP_NAME,
   )
-
-  if (steps.converting) {
-    const indexes = await read_indexes(`${RAW_LF2_PATH}/data/data.txt`);
-    await fs.rm(DATA_DIR_PATH, { recursive: true, force: true })
-    await parse_under_dir(RAW_LF2_PATH, DATA_DIR_PATH, indexes);
-    await fs.writeFile(`${DATA_DIR_PATH}/data/data.json`, JSON.stringify(indexes, null, 2).replace(/\.dat"/g, ".json\""));
-  }
-  if (steps.zipping) {
-    await make_zip_and_json(DATA_DIR_PATH, OUT_DIR, DATA_ZIP_NAME);
-    if (steps.cleanup) await fs.rm(DATA_DIR_PATH, { recursive: true, force: true })
-  }
-  await make_zip_and_json(PREL_DIR_PATH, OUT_DIR, PREL_ZIP_NAME);
-}
-
-// main();
-
-async function main2() {
-  const { RAW_LF2_PATH, DATA_DIR_PATH } = await readFile('./converter.config.json').then(buf => JSON.parse(buf.toString()))
-
   await fs.rm(DATA_DIR_PATH, { recursive: true, force: true })
-
   const ress = await classify(RAW_LF2_PATH);
-
   for (const src_path of ress.directories) {
     const dst_path = src_path.replace(RAW_LF2_PATH, DATA_DIR_PATH)
     await fs.mkdir(dst_path, { recursive: true }).catch(_ => void 0)
   }
-
   const pic_list_map = new Map<string, IEntityPictureInfo[]>();
-
   const indexes = await convert_data_txt(RAW_LF2_PATH, DATA_DIR_PATH);
   if (indexes) {
     for (const src_path of ress.file.dat) {
@@ -203,22 +86,24 @@ async function main2() {
       }
     }
   }
-  // console.log(Array.from(pic_list_map.keys()))
   for (const src_path of ress.file.bmp)
     await convert_pic(DATA_DIR_PATH, RAW_LF2_PATH, src_path, pic_list_map)
   for (const src_path of ress.file.png)
     await convert_pic(DATA_DIR_PATH, RAW_LF2_PATH, src_path, pic_list_map)
-  // for (const src_path of ress.file.wav) // .wav
-  //   await convert_sound(DATA_DIR_PATH, RAW_LF2_PATH, src_path);
-  // for (const src_path of ress.file.wma) // .wma
-  //   await convert_sound(DATA_DIR_PATH, RAW_LF2_PATH, src_path);
-  // for (const src_path of ress.unknown) {
-  //   const dst_path = src_path.replace(RAW_LF2_PATH, DATA_DIR_PATH)
-  //   console.log('copy', src_path, '=>', dst_path)
-  //   await fs.copyFile(src_path, dst_path)
-  // }
+  for (const src_path of ress.file.wav) // .wav
+    await convert_sound(DATA_DIR_PATH, RAW_LF2_PATH, src_path);
+  for (const src_path of ress.file.wma) // .wma
+    await convert_sound(DATA_DIR_PATH, RAW_LF2_PATH, src_path);
+  for (const src_path of ress.unknown) {
+    const dst_path = src_path.replace(RAW_LF2_PATH, DATA_DIR_PATH)
+    console.log('copy', src_path, '=>', dst_path)
+    await fs.copyFile(src_path, dst_path)
+  }
+  await make_zip_and_json(DATA_DIR_PATH, OUT_DIR, DATA_ZIP_NAME);
+  await fs.rm(DATA_DIR_PATH, { recursive: true, force: true })
+  await make_zip_and_json(PREL_DIR_PATH, OUT_DIR, PREL_ZIP_NAME);
 }
-main2()
+main()
 
 async function convert_pic(out_dir: string, src_dir: string, src_path: string, pic_list_map: Map<string, IEntityPictureInfo[]>) {
   if (!steps.bmp) return;
