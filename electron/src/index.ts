bf29408a27affe17bb56a86f9d985748e7feb0b0
gim_z66 <gimholg1992@foxmail.com>
2024-07-23 13:23:22 +0800

    chore: electron in

---
+++ electron/src/index.ts
@@ -0,0 +1,10 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+}
+
+app.whenReady().then(() => createWindow())
