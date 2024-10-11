c55ade44a9e060ba8f6e4f4f7047c47367a84565
gim_z66 <gimholg1992@foxmail.com>
2024-10-11 14:50:05 +0800

    。

--- tool/src/utils/file_md5_str.ts
+++ tool/src/utils/file_md5_str.ts
@@ -1,7 +1,15 @@
 import { createHash } from 'crypto';
 import fs from 'fs/promises';
 
-export async function file_md5_str(path: string) {
+/**
+ * 读取文件的md5值
+ *
+ * @export
+ * @async
+ * @param {string} path 文件目录
+ * @returns {Promise<string>} 文件内容的MD5值
+ */
+export async function file_md5_str(path: string): Promise<string> {
   const buf = await fs.readFile(path);
   return createHash('md5').update(buf).digest().toString('hex');
 }
