cb899f933c3a7255ed50c6d89d380e33f1e8175a
gim_z66 <gimholg1992@foxmail.com>
2024-10-18 15:16:36 +0800

    .

--- src/Laboratory/MNE/Interval.tsx
+++
@@ -1,9 +0,0 @@
-
-export const Interval = {
-  add(handler: () => void, timeout?: number, ...args: any[]) {
-    return window.setInterval(handler, timeout, ...args);
-  },
-  del(timer_id: number): void {
-    return window.clearInterval(timer_id);
-  }
-};
