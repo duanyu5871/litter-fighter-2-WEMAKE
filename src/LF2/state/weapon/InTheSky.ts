662c07d98e3a7fd888ee5c7e50bb56a120283d0d
gim_z66 <gimholg1992@foxmail.com>
2024-12-09 18:54:00 +0800

    weapon drop only hurt once

--- src/LF2/state/weapon/InTheSky.ts
+++ src/LF2/state/weapon/InTheSky.ts
@@ -1,11 +1,21 @@
+import { IFrameInfo } from "../../defines";
 import type Weapon from "../../entity/Weapon";
 import BaseWeaponState from "./Base";
 
 export default class InTheSky extends BaseWeaponState {
+  protected _unhurt_weapons = new Set<Weapon>();
   update(e: Weapon): void {
     e.on_gravity();
     e.velocity_decay();
     e.handle_frame_velocity();
+  }
+  enter(e: Weapon, prev_frame: IFrameInfo): void {
+    super.enter(e, prev_frame);
+    this._unhurt_weapons.add(e);
+  }
+  leave(e: Weapon, next_frame: IFrameInfo): void {
+    super.enter(e, next_frame);
+    this._unhurt_weapons.delete(e);
   }
   on_landing(e: Weapon, vx: number, vy: number, vz: number): void {
     const { base, indexes } = e.data
@@ -16,11 +26,13 @@ export default class InTheSky extends BaseWeaponState {
     } else {
       e.velocity.y = dvy;
     }
-    if (base.weapon_drop_hurt) e.hp -= base.weapon_drop_hurt;
+    if (this._unhurt_weapons.has(e)) {
+      this._unhurt_weapons.delete(e);
+      if (base.weapon_drop_hurt) e.hp -= base.weapon_drop_hurt;
+    }
     const sound_name = e.hp <= 0 ?
       base.weapon_broken_sound :
       base.weapon_drop_sound;
-
     if (sound_name) {
       const { x, y, z } = e.position;
       e.world.lf2.sounds.play(sound_name, x, y, z);
