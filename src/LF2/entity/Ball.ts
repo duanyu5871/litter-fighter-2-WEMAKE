15b389b33e95731aa92278f7d80811878dd7b3c5
gim_z66 <gimholg1992@foxmail.com>
2024-12-12 18:00:47 +0800

    .

--- src/LF2/entity/Ball.ts
+++ src/LF2/entity/Ball.ts
@@ -1,11 +1,9 @@
-import { Log } from '../../Log';
-import type { ICube, World } from '../World';
-import type { IBallData, IBallFrameInfo, IBallInfo, IBdyInfo, IItrInfo } from '../defines';
+import type { World } from '../World';
+import type { IBallData, IBallFrameInfo, IBallInfo } from '../defines';
 import { Defines } from '../defines';
 import { BALL_STATES } from '../state/ball';
 import Entity from './Entity';
 import { Factory } from './Factory';
-import { is_character, is_weapon } from './type_check';
 
 export default class Ball extends Entity<IBallFrameInfo, IBallInfo, IBallData> {
   static override readonly TAG: string = 'Ball';
@@ -38,32 +36,6 @@ export default class Ball extends Entity<IBallFrameInfo, IBallInfo, IBallData> {
       this.position.y,
       this.position.z
     )
-  }
-  override on_collision(target: Entity, itr: IItrInfo, bdy: IBdyInfo, a_cube: ICube, b_cube: ICube): void {
-    super.on_collision(target, itr, bdy, a_cube, b_cube);
-    if (is_character(target) || is_weapon(target)) {
-      const f = this.get_frame();
-      switch (itr.kind) {
-        case Defines.ItrKind.Heal:
-          if (!itr.injury) break;
-          // TODO: 
-          break;
-        case Defines.ItrKind.DeadWhenHit:
-          this.play_hit_sound()
-          if (f.on_timeout) this.enter_frame(f.on_timeout)
-          else Log.print(Ball.TAG + '::on_collision', 'Defines.ItrKind.DeadWhenHit, but on_timeout not set.')
-          break;
-        default:
-          this.play_hit_sound()
-          if (f.on_hitting) this.enter_frame(f.on_hitting);
-          else Log.print(Ball.TAG + '::on_collision', 'on_hitting not set.')
-          break;
-      }
-      if (itr.on_hit) this.enter_frame(itr.on_hit)
-      this.velocity.x = 0;
-      this.velocity.z = 0;
-      this.velocity.y = 0;
-    }
   }
   override update(): void {
     super.update();
--- src/LF2/state/ball/BaseBallState.ts
+++ src/LF2/state/ball/BaseBallState.ts
@@ -1,4 +1,8 @@
+import { Defines, IBdyInfo, IItrInfo } from "../../defines";
 import Ball from "../../entity/Ball";
+import Entity from "../../entity/Entity";
+import { is_character, is_weapon } from "../../entity/type_check";
+import { ICube } from "../../World";
 import BaseState from "../base/BaseState";
 
 export default class BaseBallState<E extends Ball = Ball> extends BaseState<E> {
@@ -41,6 +45,32 @@ export default class BaseBallState<E extends Ball = Ball> extends BaseState<E> {
       default:
         e.handle_frame_velocity();
         break;
+    }
+  }
+  // attacker
+  override on_collision(self: E, target: Entity, itr: IItrInfo, bdy: IBdyInfo, a_cube: ICube, b_cube: ICube): void {
+    if (is_character(target) || is_weapon(target)) {
+      const f = self.frame;
+      switch (itr.kind) {
+        case Defines.ItrKind.Heal:
+          if (!itr.injury) break;
+          // TODO: 
+          break;
+        case Defines.ItrKind.DeadWhenHit:
+          self.play_hit_sound()
+          if (f.on_timeout) self.enter_frame(f.on_timeout)
+          else { debugger; console.log(Ball.TAG + '::on_collision', 'Defines.ItrKind.DeadWhenHit, but on_timeout not set.') }
+          break;
+        default:
+          self.play_hit_sound()
+          if (f.on_hitting) self.enter_frame(f.on_hitting);
+          else { debugger; console.log(Ball.TAG + '::on_collision', 'on_hitting not set.') }
+          break;
+      }
+      if (itr.on_hit) self.enter_frame(itr.on_hit)
+      self.velocity.x = 0;
+      self.velocity.z = 0;
+      self.velocity.y = 0;
     }
   }
 }
