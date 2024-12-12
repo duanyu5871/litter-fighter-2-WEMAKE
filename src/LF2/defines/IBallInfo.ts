07b8e10a15ed687ff09f3c3467a0f10a8a13b92b
gim_z66 <gimholg1992@foxmail.com>
2024-12-12 11:10:51 +0800

    .

--- src/LF2/defines/IBallInfo.ts
+++ src/LF2/defines/IBallInfo.ts
@@ -1,7 +1,6 @@
 import { IGameObjInfo } from ".";
 
 export interface IBallInfo extends IGameObjInfo {
-  hp: number;
   weapon_hit_sound?: string;
   weapon_drop_sound?: string;
   weapon_broken_sound?: string;
