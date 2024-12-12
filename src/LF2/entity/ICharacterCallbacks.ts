9f7be185a17d2bccb892ffe640fcc631e45a884b
gim_z66 <gimholg1992@foxmail.com>
2024-12-12 09:37:59 +0800

    .

--- src/LF2/entity/ICharacterCallbacks.ts
+++
@@ -1,6 +0,0 @@
-// import type IEntityCallbacks from './IEntityCallbacks';
-// import type Character from './Character';
-// export default interface ICharacterCallbacks<E extends Character = Character> extends IEntityCallbacks<E> {
-
-// }
-export {}
