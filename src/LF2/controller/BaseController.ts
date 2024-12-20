5eaa8d37d88e36b0c031e2798fd36f50d50a693b
gim_z66 <gimholg1992@foxmail.com>
2024-12-20 14:46:32 +0800

    .

--- src/LF2/controller/BaseController.ts
+++ src/LF2/controller/BaseController.ts
@@ -1,3 +1,4 @@
+
 import type { IFrameInfo, IHitKeyCollection, TLooseGameKey } from '../defines';
 import { GameKey } from '../defines';
 import type Entity from '../entity/Entity';
@@ -21,6 +22,9 @@ export const CONFLICTS_KEY_MAP: Record<GameKey, GameKey | undefined> = {
   [GameKey.D]: GameKey.U,
 }
 
+/**
+ * @link https://www.processon.com/view/link/6765125f16640e2a68b21418?cid=6764eb96c3e02b46ac818e40
+ */
 export class BaseController {
   readonly is_base_controller = true
 
