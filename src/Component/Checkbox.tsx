120de81ab33db31780b6948349508f2c8960899f
gim_z66 <gimholg1992@foxmail.com>
2025-01-09 10:39:49 +0800

    .

--- src/Component/Checkbox.tsx
+++ src/Component/Checkbox.tsx
@@ -1,53 +1,30 @@
+import classNames from "classnames";
 import React, { useState } from "react";
-import "./Checkbox.css";
-import { useForwardedRef } from "./useForwardedRef";
-import { TShortcut, useShortcut } from "./useShortcut";
-export interface ICheckboxProps
-  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "value"> {
+import { Button, IButtonProps } from "./Buttons/Button";
+import styles from "./Checkbox.module.scss";
+export interface ICheckboxProps extends Omit<IButtonProps, "value"> {
   value?: boolean;
   onChanged?(v: boolean): void;
-  shortcut?: TShortcut;
-  shortcutTarget?: Window | Document | Element;
-  show_shortcut?: boolean;
 }
 export const Checkbox = React.forwardRef<HTMLButtonElement, ICheckboxProps>(
   (props: ICheckboxProps, ref: React.ForwardedRef<HTMLButtonElement>) => {
-    const {
-      value,
-      onClick,
-      onChanged,
-      shortcut,
-      shortcutTarget = window,
-      show_shortcut,
-      className,
-      ...remain_props
-    } = props;
+    const { value, onClick, onChanged, className, ...remain_props } = props;
     const [_value, _set_value] = useState<boolean | undefined>(value);
-
     const _on_click = (e: React.MouseEvent<HTMLButtonElement>) => {
       onClick?.(e);
       _set_value((v) => !v);
       onChanged?.(!value);
     };
-
-    const [ref_btn, on_ref] = useForwardedRef<HTMLButtonElement>(ref);
-    useShortcut(shortcut, props.disabled, ref_btn, shortcutTarget);
-    const root_className = className
-      ? `lfui_button lf2ui_checkbox ${className}`
-      : "lfui_button lf2ui_checkbox";
-    const inner_className =
-      (value ?? _value)
-        ? "lf2ui_checkbox_inner_show"
-        : "lf2ui_checkbox_inner_hide";
+    const checked = value ?? _value;
+    const root_className = classNames(styles.lf2ui_checkbox, className)
+    const inner_className = classNames(styles.lf2ui_checkbox, {
+      [styles.inner_show]: checked,
+      [styles.inner_hide]: !checked,
+    })
     return (
-      <button
-        {...remain_props}
-        className={root_className}
-        ref={on_ref}
-        onClick={_on_click}
-      >
+      <Button {...remain_props} className={root_className} onClick={_on_click} _ref={ref}>
         <span className={inner_className}>✓</span>
-      </button>
+      </Button>
     );
   },
 );
