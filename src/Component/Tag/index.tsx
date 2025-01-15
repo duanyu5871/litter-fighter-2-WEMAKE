884f36a3f6bdbb3ce70e793d947f329e399884cc
gim_z66 <gimholg1992@foxmail.com>
2025-01-15 18:38:48 +0800

    .

--- src/Component/Tag/index.tsx
+++ src/Component/Tag/index.tsx
@@ -7,17 +7,18 @@ import styles from "./style.module.scss";
 export interface ITagProps extends ISpaceProps {
   closeable?: boolean;
   on_close?: React.PointerEventHandler<SVGSVGElement>;
-  size?: Text.UiSize
+  size?: Text.UiSize;
+  close_size?: Text.UiSize;
 }
 export function Tag(props: ITagProps) {
-  const { children, className, closeable, on_close, ..._p } = props;
+  const { children, className, closeable, size = 's', close_size = 'ss', on_close, ..._p } = props;
   const cls_name = classNames(styles.lfui_tag, { [styles.closeable]: closeable }, className)
   return (
     <Space className={cls_name} {..._p}>
       <Space.Broken>
-        <Text size='s'>{children}</Text>
+        <Text size={size}>{children}</Text>
         {closeable ?
-          <Text size='ss' className={styles.close}>
+          <Text size={close_size} className={styles.close}>
             <Cross hoverable onPointerDown={on_close} />
           </Text>
           : null}
