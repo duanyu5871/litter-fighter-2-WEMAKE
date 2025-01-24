49393dd90988f77f957755861672e8a01f59e06e
gim_z66 <gimholg1992@foxmail.com>
2025-01-24 15:47:02 +0800

    .

--- src/LF2/ditto/IVector2.ts
+++ src/LF2/ditto/IVector2.ts
@@ -2,7 +2,9 @@ export interface IVector2 {
   x: number;
   y: number;
   set(x: number, y: number): void;
-  add(o: IVector2): void;
+  add(o: IVector2): IVector2;
+  sub(o: IVector2): IVector2;
+  length(): number;
   clone(): IVector2;
   normalize(): this;
 }
