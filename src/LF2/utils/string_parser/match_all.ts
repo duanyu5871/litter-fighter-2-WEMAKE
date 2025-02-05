0dd42578e271c932ef3b19811e3c12c4d414bd1c
gim <gimholg1992@foxmail.com>
2025-02-05 19:51:16 +0800

    。

--- src/LF2/utils/string_parser/match_all.ts
+++ src/LF2/utils/string_parser/match_all.ts
@@ -1,4 +1,22 @@
+
+/**
+ * 匹配字符串中符合正则表达式的段落，返回匹配结果
+ *
+ * @export
+ * @param {string} text 
+ * @param {RegExp} regexp 
+ * @returns {RegExpExecArray[]} 
+ */
 export function match_all(text: string, regexp: RegExp): RegExpExecArray[];
+
+/**
+ * 匹配字符串中符合正则表达式的段落，并一一处理
+ *
+ * @export
+ * @param {string} text 
+ * @param {RegExp} regexp 
+ * @param {(result: RegExpExecArray) => void} func 处理回调，每条符合
+ */
 export function match_all(
   text: string,
   regexp: RegExp,
