7468ab41e51152b32d26bc359b0b35b9611676c0
gim <gimholg1992@foxmail.com>
2025-06-30 19:21:53 +0800

    .

--- src/LF2/dat_translator/make_ball_data.ts
+++ src/LF2/dat_translator/make_ball_data.ts
@@ -184,6 +184,12 @@ export function make_ball_data(
           y: frame.centery,
           dvx: 8,
           action: { id: "50" },
+        },{
+          kind: OpointKind.Normal,
+          oid: BuiltIn_OID.JulianBall,
+          x: frame.centerx,
+          y: frame.centery,
+          action: { id: "10" },
         });
         break;
       case FrameBehavior.JulianBall:
